e6e770de9784cf764a3c5405db3ccbeb73a0b80e
Ushima <dmejiaus@cesde.net>
2023-10-02 14:34:48 -0500

    Agregados productos prueba, funcion search

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,7 +1,148 @@
 // CREAR EL ARREGLO DE OBJETOS PARA EL CARRITO DE COMPRAS
 let cart = [];
 //Crear el arreglo de objetos de los productos
-let products = [];
+let products = [
+  {
+    idprod: "1",
+    prodname: "Laptop",
+    prodimage: "laptop.jpg",
+    prodprice: "1000",
+    prodstatus: true,
+  },
+  {
+    idprod: "2",
+    prodname: "Teléfono",
+    prodimage: "phone.jpg",
+    prodprice: "500",
+    prodstatus: true,
+  },
+  {
+    idprod: "3",
+    prodname: "Tablet",
+    prodimage: "tablet.jpg",
+    prodprice: "300",
+    prodstatus: false,
+  },
+  {
+    idprod: "4",
+    prodname: "Reloj",
+    prodimage: "watch.jpg",
+    prodprice: "200",
+    prodstatus: true,
+  },
+  {
+    idprod: "5",
+    prodname: "Cámara",
+    prodimage: "camera.jpg",
+    prodprice: "400",
+    prodstatus: true,
+  },
+  {
+    idprod: "6",
+    prodname: "Auriculares",
+    prodimage: "headphones.jpg",
+    prodprice: "50",
+    prodstatus: false,
+  },
+  {
+    idprod: "7",
+    prodname: "Teclado",
+    prodimage: "keyboard.jpg",
+    prodprice: "30",
+    prodstatus: true,
+  },
+  {
+    idprod: "8",
+    prodname: "Ratón",
+    prodimage: "mouse.jpg",
+    prodprice: "20",
+    prodstatus: true,
+  },
+  {
+    idprod: "9",
+    prodname: "Monitor",
+    prodimage: "monitor.jpg",
+    prodprice: "150",
+    prodstatus: true,
+  },
+  {
+    idprod: "10",
+    prodname: "Impresora",
+    prodimage: "printer.jpg",
+    prodprice: "100",
+    prodstatus: false,
+  },
+  {
+    idprod: "11",
+    prodname: "Silla",
+    prodimage: "chair.jpg",
+    prodprice: "70",
+    prodstatus: true,
+  },
+  {
+    idprod: "12",
+    prodname: "Altavoces",
+    prodimage: "speakers.jpg",
+    prodprice: "80",
+    prodstatus: true,
+  },
+  {
+    idprod: "13",
+    prodname: "Micrófono",
+    prodimage: "mic.jpg",
+    prodprice: "40",
+    prodstatus: true,
+  },
+  {
+    idprod: "14",
+    prodname: "Router",
+    prodimage: "router.jpg",
+    prodprice: "60",
+    prodstatus: false,
+  },
+  {
+    idprod: "15",
+    prodname: "Disco duro",
+    prodimage: "hdd.jpg",
+    prodprice: "120",
+    prodstatus: true,
+  },
+  {
+    idprod: "16",
+    prodname: "SSD",
+    prodimage: "ssd.jpg",
+    prodprice: "150",
+    prodstatus: true,
+  },
+  {
+    idprod: "17",
+    prodname: "Tarjeta gráfica",
+    prodimage: "gpu.jpg",
+    prodprice: "300",
+    prodstatus: true,
+  },
+  {
+    idprod: "18",
+    prodname: "RAM",
+    prodimage: "ram.jpg",
+    prodprice: "90",
+    prodstatus: false,
+  },
+  {
+    idprod: "19",
+    prodname: "Placa madre",
+    prodimage: "motherboard.jpg",
+    prodprice: "110",
+    prodstatus: true,
+  },
+  {
+    idprod: "20",
+    prodname: "Fuente de poder",
+    prodimage: "psu.jpg",
+    prodprice: "80",
+    prodstatus: true,
+  },
+];
 
 // REFERENCIAR TODOS LOS ELEMENTOS DE
 
@@ -45,7 +186,7 @@ function addProduct(idprod, prodname, prodimage, prodprice, prodstatus) {
 }
 
 function searchProduct(value) {
-  return products.filter((product) => {
+  let foundProducts = products.filter((product) => {
     for (const key in value) {
       if (!product.hasOwnProperty(key) || product[key] !== value[key]) {
         return false;
@@ -53,7 +194,29 @@ function searchProduct(value) {
     }
     return true;
   });
+
+  if (foundProducts.length > 0) {
+    // Mostrar el primer producto encontrado en los campos
+    const { idprod, prodname, prodimage, prodprice, prodstatus } =
+      foundProducts[0];
+    idProd.value = idprod;
+    prodName.value = prodname;
+    prodImage.value = prodimage;
+    prodPrice.value = prodprice;
+    prodStatus.value = prodstatus ? "enable" : "disable";
+
+    // Si hay más de un producto encontrado, mostrarlos en una tabla
+    if (foundProducts.length > 1) {
+      displayTableResults(foundProducts.slice(1));
+    }
+  } else {
+    message.classList.remove("alert-success");
+    message.classList.remove("d-none");
+    message.classList.add("alert-danger");
+    message.textContent = "Producto no encontrado. Inténtelo de nuevo.";
+  }
 }
+
 /*
 function updateProduct(idprod){}
 function deleteProduct(idprod){}
@@ -86,6 +249,10 @@ btnSave.addEventListener("click", () => {
     message.classList.remove("alert-success");
     message.classList.add("alert-danger");
     message.textContent = " Debe ingresar todos los datos...";
+    setTimeout(() => {
+      message.textContent = "";
+      message.classList.add("d-none");
+    }, 3000);
   }
 });
 
@@ -117,3 +284,22 @@ btnSearch.addEventListener('click',()=>{
 btnUpdate.addEventListener("click", () => {
   updateProduct(idProd.value);
 });
+
+function displayTableResults(results) {
+  let tableHTML = '<table class="table">'; // Aquí se define y se agrega la clase de Bootstrap
+  tableHTML +=
+    "<thead><tr><th>ID</th><th>Name</th><th>Image</th><th>Price</th><th>Status</th></tr></thead>";
+  tableHTML += "<tbody>";
+  results.forEach((result) => {
+    tableHTML += `<tr>
+          <td>${result.idprod}</td>
+          <td>${result.prodname}</td>
+          <td>${result.prodimage}</td>
+          <td>${result.prodprice}</td>
+          <td>${result.prodstatus ? "enable" : "disable"}</td>
+      </tr>`;
+  });
+  tableHTML += "</tbody></table>";
+
+  document.getElementById("resultsTable").innerHTML = tableHTML;
+}
